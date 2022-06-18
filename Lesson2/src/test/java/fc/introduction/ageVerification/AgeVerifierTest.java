9b1a3b78e27377e05f07f83a71816ce9731c42bf
Krzysztof Czerski <krzysztof.czerskip@gmail.com>
2022-06-18 17:15:57 +0200

    Lesson3 Fixed class and test

--- Lesson2/src/test/java/fc/introduction/ageVerification/AgeVerifierTest.java
+++ Lesson2/src/test/java/fc/introduction/ageVerification/AgeVerifierTest.java
@@ -6,59 +6,44 @@ import org.junit.jupiter.api.Test;
 class AgeVerifierTest {
 
     @Test
-    public void shouldPrintCorrectResultWhenUserAgeIsHigher() {
+    public void shouldReturnCorrectResultWhenUserAgeIsHigher() {
 
         //given
         int firstUserAge = 50;
-        int secondUserAge = 20;
-        int thirdUserAge = 19;
 
         //when
         AgeVerifier ageVerifier = new AgeVerifier();
-        String firstAgeNumber = ageVerifier.ageVerify(firstUserAge);
-        String secondAgeNumber = ageVerifier.ageVerify(secondUserAge);
-        String thirdAgeNumber = ageVerifier.ageVerify(thirdUserAge);
+        String firstAgeNumber = ageVerifier.verifyAge(firstUserAge);
 
         //then
         Assertions.assertEquals(firstAgeNumber,"User's age is more than 18 years old.");
-        Assertions.assertEquals(secondAgeNumber,"User's age is more than 18 years old.");
-        Assertions.assertEquals(thirdAgeNumber,"User's age is more than 18 years old.");
     }
 
     @Test
-    public void shouldPrintCorrectResultWhenUserAgeIsLess() {
+    public void shouldReturnCorrectResultWhenUserAgeIsLess() {
 
         //given
         int firstUserAge = 15;
-        int secondUserAge = 14;
-        int thirdUserAge = 2;
 
         //when
         AgeVerifier ageVerifier = new AgeVerifier();
-        String firstAgeNumber = ageVerifier.ageVerify(firstUserAge);
-        String secondAgeNumber = ageVerifier.ageVerify(secondUserAge);
-        String thirdAgeNumber = ageVerifier.ageVerify(thirdUserAge);
+        String firstAgeNumber = ageVerifier.verifyAge(firstUserAge);
 
         //then
         Assertions.assertEquals(firstAgeNumber,"User's age is less than 18 years old.");
-        Assertions.assertEquals(secondAgeNumber,"User's age is less than 18 years old.");
-        Assertions.assertEquals(thirdAgeNumber,"User's age is less than 18 years old.");
     }
 
     @Test
-    public void shouldPrintCorrectResultWhenUserAgeIsEqual() {
+    public void shouldReturnCorrectResultWhenUserAgeIsEqual() {
 
         //given
         int firstUserAge = 18;
-        int secondUserAge = 18;
 
         //when
         AgeVerifier ageVerifier = new AgeVerifier();
-        String firstAgeNumber = ageVerifier.ageVerify(firstUserAge);
-        String secondAgeNumber = ageVerifier.ageVerify(secondUserAge);
+        String firstAgeNumber = ageVerifier.verifyAge(firstUserAge);
 
         //then
         Assertions.assertEquals(firstAgeNumber,"User's age is 18 years old.");
-        Assertions.assertEquals(secondAgeNumber,"User's age is 18 years old.");
     }
 }
