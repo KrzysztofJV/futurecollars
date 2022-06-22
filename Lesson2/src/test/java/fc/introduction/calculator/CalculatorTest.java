54c255420234b10566906728f6deda0d32b75d73
Krzysztof Czerski <krzysztof.czerskip@gmail.com>
2022-06-22 20:49:44 +0200

    Lesson3 Renamed methods and removed redundant code in Test

--- Lesson2/src/test/java/fc/introduction/calculator/CalculatorTest.java
+++ Lesson2/src/test/java/fc/introduction/calculator/CalculatorTest.java
@@ -6,82 +6,62 @@ import org.junit.jupiter.api.Test;
 class CalculatorTest {
 
     @Test
-    public void sum() {
+    public void shouldAdd() {
 
         //given
         double firstNumber=2;
         double secondNumber=3;
-        double thirdNumber=4;
 
         //when
         Calculator calculator = new Calculator();
-        double sum1 = calculator.sum(firstNumber, secondNumber);
-        double sum2 = calculator.sum(firstNumber, thirdNumber);
-        double sum3 = calculator.sum(secondNumber, thirdNumber);
+        double result = calculator.sum(firstNumber, secondNumber);
 
         //then
-        Assertions.assertEquals(5,sum1);
-        Assertions.assertEquals(6,sum2);
-        Assertions.assertEquals(7,sum3);
+        Assertions.assertEquals(5,result);
     }
 
     @Test
-    public void subtract() {
+    public void shouldSubtract() {
 
         //given
         double firstNumber=2;
         double secondNumber=3;
-        double thirdNumber=4;
 
         //when
         Calculator calculator = new Calculator();
-        double sub1 = calculator.subtract(firstNumber, secondNumber);
-        double sub2 = calculator.subtract(firstNumber, thirdNumber);
-        double sub3 = calculator.subtract(secondNumber, thirdNumber);
+        double result = calculator.subtract(firstNumber, secondNumber);
 
         //then
-        Assertions.assertEquals(-1,sub1);
-        Assertions.assertEquals(-2,sub2);
-        Assertions.assertEquals(-1,sub3);
+        Assertions.assertEquals(-1,result);
     }
 
     @Test
-    void multiply() {
+    void shouldMultiply() {
 
         //given
         double firstNumber=2;
         double secondNumber=3;
-        double thirdNumber=4;
 
         //when
         Calculator calculator = new Calculator();
-        double mul1 = calculator.multiply(firstNumber, secondNumber);
-        double mul2 = calculator.multiply(firstNumber, thirdNumber);
-        double mul3 = calculator.multiply(secondNumber, thirdNumber);
+        double result = calculator.multiply(firstNumber, secondNumber);
 
         //then
-        Assertions.assertEquals(6,mul1);
-        Assertions.assertEquals(8,mul2);
-        Assertions.assertEquals(12,mul3);
+        Assertions.assertEquals(6,result);
     }
 
     @Test
-    public void divide() {
+    public void shouldDivide() {
 
         //given
         double firstNumber=2;
         double secondNumber=3;
-        double thirdNumber=4;
 
         //when
         Calculator calculator = new Calculator();
-        double div1 = calculator.divide(firstNumber, secondNumber);
-        double div2 = calculator.divide(firstNumber, thirdNumber);
-        double div3 = calculator.divide(secondNumber, thirdNumber);
+        double result = calculator.divide(firstNumber, secondNumber);
 
         //then
-        Assertions.assertEquals(0.6666666666666666,div1);
-        Assertions.assertEquals(0.5,div2);
-        Assertions.assertEquals(0.75,div3);
+        Assertions.assertEquals(0.6666666666666666,result);
     }
 }
