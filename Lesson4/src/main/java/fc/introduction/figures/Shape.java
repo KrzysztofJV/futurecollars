69c2466f7c11d0ea29f1da2d89a4d756f979daa6
Krzysztof Czerski <krzysztof.czerskip@gmail.com>
2022-06-24 22:50:45 +0200

    Lesson4 Final version Figures Task

--- Lesson4/src/main/java/fc/introduction/figures/Shape.java
+++ Lesson4/src/main/java/fc/introduction/figures/Shape.java
@@ -5,9 +5,4 @@ public abstract class Shape {
     public abstract double getArea();
     public abstract double getPerimeter();
 
-
-    public static void main(String[] args) {
-        Square square = new Square(-4);
-        System.out.println(square.getArea());
-    }
 }
