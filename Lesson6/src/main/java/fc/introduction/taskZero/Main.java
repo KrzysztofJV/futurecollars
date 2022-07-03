5c00603e71feec854fed56c29c32563366fad301
Krzysztof Czerski <krzysztof.czerskip@gmail.com>
2022-07-03 17:59:49 +0200

    Lesson6 Task0

---
+++ Lesson6/src/main/java/fc/introduction/taskZero/Main.java
@@ -0,0 +1,29 @@
+package fc.introduction.taskZero;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\krzys\\Desktop\\futurecollars\\Lesson6\\src\\main\\java\\fc\\introduction\\taskZero\\data.txt");
+
+        writeToFile(file.getPath(), "Kurs\n" +
+                "Java\n" +
+                "Lekcja 6\n" +
+                "Pliki\n" +
+                "Wyjątki\n" +
+                "Pliki\n" +
+                "Koniec pliku");
+    }
+
+    public static void writeToFile(String pathToFile, String text) {
+        try {
+            Files.writeString(Paths.get(pathToFile), text);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
