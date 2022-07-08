05ef6543e959130b0fb220691db64f690d6ea165
Krzysztof Czerski <krzysztof.czerskip@gmail.com>
2022-07-08 23:09:44 +0200

    Lesson6-Task7

--- Lesson6CD/src/main/java/fc/introduction/task7/Main.java
+++ Lesson6CD/src/main/java/fc/introduction/task7/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         SquareRootOfTheNumberPrinter squareRootOfTheNumberPrinter = new SquareRootOfTheNumberPrinter();
-        double result = squareRootOfTheNumberPrinter.printSquareRootOfTheGivenNumber(-4);
+        double result = squareRootOfTheNumberPrinter.printSquareRootOfTheGivenNumber(4);
         System.out.println(result);
     }
 }
