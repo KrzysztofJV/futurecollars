a8d56acc6b3cfa7734b3a3d6f225b33eeacb1442
Krzysztof Czerski <krzysztof.czerskip@gmail.com>
2022-07-10 21:26:00 +0200

    Lesson6-Task8

--- Lesson5/src/main/java/fc/introduction/calculator/Multiply.java
+++
@@ -1,9 +0,0 @@
-package fc.introduction.calculator;
-
-public class Multiply implements Operation{
-
-    @Override
-    public double calculate(double firstValue, double secondValue) {
-        return firstValue*secondValue;
-    }
-}
--- Lesson5/src/main/java/fc/introduction/fizzBuzz/Main.java
+++
@@ -1,9 +0,0 @@
-package fc.introduction.fizzBuzz;
-
-public class Main {
-
-    public static void main(String[] args) {
-        FizzBuzz fizzBuzz = new FizzBuzz(1, 100);
-        fizzBuzz.getFizzBuzz();
-    }
-}
