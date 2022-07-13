4527d99ce6c3ccaed3f2b1900d8e13d6e4244993
Krzysztof Czerski <krzysztof.czerskip@gmail.com>
2022-07-13 21:47:52 +0200

    Lesson7-Task4

---
+++ Lesson7/src/test/java/com/futurecollars/taskFour/TextLengthTest.java
@@ -0,0 +1,25 @@
+package com.futurecollars.taskFour;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextLengthTest {
+
+    private static Stream<Arguments> provideStringsForGetTextLength() {
+        return Stream.of(
+                Arguments.of("Java", 4),
+                Arguments.of("Introduction to Java", 20),
+                Arguments.of("", 0),
+                Arguments.of("5 x 5", 5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForGetTextLength")
+    void shouldReturnTextLength(String text, int lengthExpected) {
+        assertEquals(lengthExpected, TextLength.getTextLength(text));
+    }
+}
