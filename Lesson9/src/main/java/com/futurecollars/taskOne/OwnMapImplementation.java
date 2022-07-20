4f59708a52dc5c88930ba0c181b75040beb44aac
Krzysztof Czerski <krzysztof.czerskip@gmail.com>
2022-07-20 18:38:47 +0200

    Lesson9-Task1

---
+++ Lesson9/src/main/java/com/futurecollars/taskOne/OwnMapImplementation.java
@@ -0,0 +1,47 @@
+package com.futurecollars.taskOne;
+
+public class OwnMapImplementation implements OwnMap {
+
+    private final int HASH_MAP_SIZE = 1000;
+    private final String[] arrayString = new String[HASH_MAP_SIZE];
+
+    private int hash(String key) {
+        return key.hashCode() % HASH_MAP_SIZE;
+    }
+
+
+    @Override
+    public boolean put(String key, String value) {
+        arrayString[hash(key)] = value;
+        return true;
+    }
+
+    @Override
+    public boolean containsKey(String key) {
+        return arrayString[hash(key)] != null;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        boolean isFound = false;
+        for (int index = 0; index < HASH_MAP_SIZE; index++) {
+            if (arrayString[index] == value) {
+                isFound = true;
+                break;
+            }
+        }
+        return isFound;
+    }
+
+    @Override
+    public String remove(String key) {
+        String element = arrayString[hash(key)];
+        arrayString[hash(key)] = null;
+        return element;
+    }
+
+    @Override
+    public String get(String key) {
+        return arrayString[hash(key)];
+    }
+}
