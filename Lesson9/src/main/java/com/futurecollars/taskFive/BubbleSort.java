7bd0c9372e53da454c125b3c0769f4852c0adcf1
Krzysztof Czerski <krzysztof.czerskip@gmail.com>
2022-07-21 15:29:41 +0200

    Lesson9-Task5

---
+++ Lesson9/src/main/java/com/futurecollars/taskFive/BubbleSort.java
@@ -0,0 +1,19 @@
+package com.futurecollars.taskFive;
+
+public class BubbleSort {
+
+    public <E extends Comparable<E>>
+    E[] sortArray(E[] list) {
+        E temp;
+        for (int i = 0; i < list.length - 1; i++) {
+            for (int j = 0; j < list.length - 1 - i; j++) {
+                if (list[j].compareTo(list[j + 1]) > 0) {
+                    temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                }
+            }
+        }
+        return list;
+    }
+}
