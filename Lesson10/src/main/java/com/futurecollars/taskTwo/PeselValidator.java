c9a1b8d68014c425c9fd24dcc4b80d93b2aa114c
Krzysztof Czerski <krzysztof.czerskip@gmail.com>
2022-07-22 17:52:09 +0200

    Lesson10-Task2

---
+++ Lesson10/src/main/java/com/futurecollars/taskTwo/PeselValidator.java
@@ -0,0 +1,13 @@
+package com.futurecollars.taskTwo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PeselValidator {
+
+    public static boolean checkPesel(String pesel) {
+        Pattern pattern = Pattern.compile("\\d{11}");
+        Matcher matcher = pattern.matcher(pesel);
+        return matcher.matches();
+    }
+}
